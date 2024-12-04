4514845e54f0665671d8a93e20ec9cd61f035517
Anshul Jain <anshul.jain@sierralivingconcepts.com>
2024-12-04 11:43:28 +0530

    Initial Client app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,25 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,NgFor],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   http = inject(HttpClient);
   title = 'datingapp';
+  users : any;
 
 ngOnInit():void{
-  this.http.get("http://localhost:5152/api/users");
+  this.http.get("http://localhost:5152/api/users").subscribe({
+    next:response=> this.users=response,
+    error:error=>console.log(error),
+    complete:()=>console.log('requiest has been completed!!!')
+  });
 }
 }
